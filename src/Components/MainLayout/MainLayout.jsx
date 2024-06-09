434283aa94aaf194c2137f5432e76a61ef06f813
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-09 18:49:00 +0600

    home route created

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Home/Header/Header";
 
 
 const MainLayout = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
