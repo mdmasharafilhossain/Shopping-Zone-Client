c0e2cf06cc5a26b00f8ec613aabc19527059c78b
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-09 22:24:35 +0600

    react icon installed

--- src/Components/Home/Header2/Header2.jsx
+++ src/Components/Home/Header2/Header2.jsx
@@ -1,16 +1,20 @@
-import logo from '../../../assets/website_logo.png'
-
+import logo from "../../../assets/website_logo.png";
+import { FaSearch } from "react-icons/fa";
 const Header2 = () => {
-    return (
-        <div className="flex border">
-          <div>
-             <img className='w-40' src={logo} alt="" />
-          </div>
-          <div>
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-          </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center border p-2">
+      <div>
+        <img className="w-44" src={logo} alt="Website Logo" />
+      </div>
+      <div className="flex ml-4">
+        <input type="text" placeholder="Search For Products" className="border rounded-l px-4 py-2 focus:border-orange-500 focus:outline-none"
+        />
+        <button className="bg-orange-500 text-white px-4 py-2 rounded-r">
+          
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header2;
