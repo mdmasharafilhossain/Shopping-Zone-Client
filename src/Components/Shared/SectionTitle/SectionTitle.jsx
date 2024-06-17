b2859c7c195247da7e029b388b79202a9978c318
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-17 21:41:04 +0600

    All Categories route created

--- src/Components/Shared/SectionTitle/SectionTitle.jsx
+++ src/Components/Shared/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@
 const SectionTitle = ({heading}) => {
     return (
         <div>
-           <h1>{heading}</h1> 
+           <h1 className="border-l-4 border-orange-500 text-3xl mt-20 pl-4 font-bold">{heading}</h1> 
         </div>
     );
 };
