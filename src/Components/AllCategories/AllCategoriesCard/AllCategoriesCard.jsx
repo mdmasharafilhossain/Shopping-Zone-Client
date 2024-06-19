d28f084c117084796c7f3e315befb0d35890c7da
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-19 09:39:27 +0600

    bug fixed on category section

--- src/Components/AllCategories/AllCategoriesCard/AllCategoriesCard.jsx
+++ src/Components/AllCategories/AllCategoriesCard/AllCategoriesCard.jsx
@@ -5,12 +5,12 @@ const AllCategoriesCard = ({categorie}) => {
     const {image,category,title,} = categorie || {}
 
     return (
-        <div>
+        <div className="mt-10 mb-20 mr-5">
             <Link to={`/${category}`}>
-            <div className="border-[1px]  hover:border-[1px] hover:shadow-xl hover:border-gray-200 card card-compact  ">
-                <figure><img src={image} alt="" /></figure>
+            <div className="border-[1px]  hover:border-[1px] hover:shadow-xl hover:border-gray-200 card card-compact h-60  ">
+                <figure><img className="w-60 h-40" src={image} alt="" /></figure>
                 <div className="card-body">
-                    <h2 className="text-lg font-medium">{title}</h2>
+                    <h2 className="text-lg font-medium text-center">{title}</h2>
                 </div>
             </div>
         </Link>
