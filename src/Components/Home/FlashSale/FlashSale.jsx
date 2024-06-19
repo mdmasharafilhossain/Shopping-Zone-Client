bd2e68432c1bf818f2858da0ddd275501ece2700
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-19 12:02:40 +0600

    react count down added

---
+++ src/Components/Home/FlashSale/FlashSale.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+
+
+const FlashSale = () => {
+    return (
+        <div className="ml-10 mr-5">
+            <SectionTitle heading={"Flash Sale"}>
+                
+            </SectionTitle>
+            <hr className="mt-5 "/>
+        </div>
+    );
+};
+
+export default FlashSale;
