bc766c6fae098101193e58025e200e114ca3f24d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-21 18:33:36 +0600

    image zoom componenet added

--- src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
+++ src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
@@ -1,19 +1,20 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import ImageZoom from "./ImageZoom"; // Make sure the path is correct
 
 const FlashSaleDetails = () => {
     const CardsInfo = useLoaderData();
-    console.log(CardsInfo);
     const { id } = useParams();
-    console.log("id",id)
-    
-    const InfoCard = CardsInfo.result.find(brand =>brand._id === id);
-    
-    console.log(InfoCard);
+
+    const InfoCard = CardsInfo.result.find(brand => brand._id === id);
+
+    const DiscountPercentage = Math.round(((InfoCard.price - InfoCard.discount_price) / InfoCard.price) * 100);
+
     return (
-        <div>
-            <h1 className="mt-20">{InfoCard.length}</h1>
-            <img  src={InfoCard.image} alt="" />
+        <div className="flex mt-40 ml-40">
+            <div className="relative w-52 h-80 mr-10">
+                <ImageZoom src={InfoCard.image} alt={InfoCard.name} />
+              
+            </div>
         </div>
     );
 };
---
+++ src/Components/Home/FlashSale/FlasSaleDetails/ImageZoom.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const ImageZoom = ({ src, alt }) => {
+    const [isZoomed, setIsZoomed] = useState(false);
+    const [backgroundPosition, setBackgroundPosition] = useState('0% 0%');
+
+    const handleMouseEnter = () => {
+        setIsZoomed(true);
+    };
+
+    const handleMouseMove = (e) => {
+        const { left, top, width, height } = e.target.getBoundingClientRect();
+        const x = ((e.pageX - left) / width) * 100;
+        const y = ((e.pageY - top) / height) * 100;
+        setBackgroundPosition(`${x}% ${y}%`);
+    };
+
+    const handleMouseLeave = () => {
+        setIsZoomed(false);
+    };
+
+    return (
+        <div className="relative w-full h-full" onMouseEnter={handleMouseEnter} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+            <img className="w-full h-full object-cover" src={src} alt={alt} />
+            {isZoomed && (
+                <div className="absolute inset-y-0 left-full w-80 h-80 bg-no-repeat z-10 border border-gray-300" style={{ backgroundImage: `url(${src})`, backgroundSize: '200%', backgroundPosition: backgroundPosition }}></div>
+            )}
+        </div>
+    );
+};
+
+export default ImageZoom;
