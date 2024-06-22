6581c343f98fcfffb0ce44c80978babcf42a0ec1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-22 15:26:49 +0600

    add review section added

--- src/Components/Home/FlashSale/AddComment/AddComment.jsx
+++ src/Components/Home/FlashSale/AddComment/AddComment.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const AddComment = ({InfoCard}) => {
     const [reviews, setReviews] = useState([]);
     const [rating, setRating] = useState('');
-    const {name,image} = InfoCard || {}
+    const formRef = useRef(null);
+    const {name,code} = InfoCard || {}
+    
+    
+    
+    const handleSubmit = e =>{
+        e.preventDefault();
+        const form = e.target;
+        
+        const name = form.name.value;
+        const photo = form.image.value;
+        const customer_rating = rating;
+        const review = form.rating.value;
+        
+        const UpdateProducts = {name,photo,customer_rating,review,code}
+        console.log(UpdateProducts);
+        
 
+        
+        //   axiosSecure.put(`/users/profile/${user.email}`,UpdateProducts)
+        //   .then(res=>{
+        //     console.log(res.data);
+        //     if(res.data.modifiedCount > 0){
+        //         refetch();
+        //         Swal.fire({
+        //             position: "top-end",
+        //             icon: "success",
+        //             title: `Update Successfully`,
+        //             showConfirmButton: false,
+        //             timer: 1500
+        //           });
+        //     }
+        //  })
+     e.target.reset();
+    }
     return (
         <div className=" mt-10 mx-10 ">
-            <div className="border flex justify-between p-2">
+            <div >
+                <div className="border flex justify-between p-2">
                 <h1 className="text-xl font-bold">Reviews</h1>
                 <div>
                 <button className="text-xs p-2 border rounded-md hover:bg-orange-500 hover:text-white" onClick={() => document.getElementById('my_modal_1').showModal()}>Write a review</button> 
+                </div>
                 <dialog id="my_modal_1" className="modal">
                         <div className="modal-box">
-                        <form onSubmit="{hadndleUpdate}">
-                        <div className="flex flex-col md:flex-row lg:flex-row gap-5">
+                        <form onSubmit={handleSubmit}>
+                        <div className=" space-y-4">
+                            <h1 className='text-2xl text-orange-500 mb-5 '>Write a Review</h1>
+                            {/* Name and Rating div */}
+                        <div className='flex flex-col md:flex-row lg:flex-row gap-5'>
                         <div className="form-control w-3/4 md:w-1/2">
                             <label className="label">
                                 <span className="label-text">Product Name</span>
                             </label>
-                            <label className="input-group">
+                            <label className="input-group ">
 
-                                <input type="text" readOnly defaultValue={name} placeholder="Name" name="name" className="input input-bordered w-full" />
+                                <input type="text"  readOnly defaultValue={name} placeholder="Name" name="name" className="input input-bordered w-full  outline-none" />
                             </label>
                         </div>
                         {/* Rating div */}
-                        <div className="form-control w-3/4 md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Rating</span>
+                        <div className="form-control w-3/4 md:w-1/2 mt-3">
+                            <label className="label ">
+                                <span className="label-text mr-40">Rating*</span>
                             </label>
                             <div className="flex space-x-4">
             <label>
@@ -53,9 +91,28 @@ const AddComment = ({InfoCard}) => {
             </label>
           </div>
                         </div>
+                        </div>
+                {/* Text Area  */}
+                <div>
+                <label className="form-control">
+  <div className="label">
+    <span className="label-text">Write Your Review</span>
+   
+  </div>
+  <textarea className="textarea textarea-bordered h-24" name="rating" placeholder="Write Your Review"></textarea>
+  <div className="label">
+    
+  </div>
+</label>
+                </div>
+                {/* File input */}
+                <div>
+                    
+                <input type="file" name="image" className="file-input file-input-ghost w-full max-w-xs" />
+                </div>
                         
                     </div>
-                    <button className="btn mt-5 ml-48">Update</button>
+                    <button className="btn mt-5 ml-48">Submit</button>
                         </form>
                             <div className="modal-action">
                                 <form method="dialog">
--- src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
+++ src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
@@ -6,28 +6,28 @@ const FlashSaleDetails = () => {
     const CardsInfo = useLoaderData();
     const { id } = useParams();
 
-    const InfoCard = CardsInfo.result.find(brand => brand._id === id);
+    const InfoCard = CardsInfo?.result?.find(brand => brand._id === id);
 
-    const DiscountPercentage = Math.round(((InfoCard?.price - InfoCard?.discount_price) / InfoCard.price) * 100);
+    const DiscountPercentage = Math.round(((InfoCard?.price - InfoCard?.discount_price) / InfoCard?.price) * 100);
 
     return (
         <div>
             <div className="flex mt-40 ml-40">
             <div className="relative mr-10 ">
             <div className="block lg:hidden">
-                    <img className="w-80 h-full object-cover" src={InfoCard.image} alt={InfoCard.name} />
+                    <img className="w-80 h-full object-cover" src={InfoCard?.image} alt={InfoCard?.name} />
                 </div>
                 <div className="hidden lg:block w-80 h-96 border hover:border-orange-500 p-5">
-                    <ImageZoom className=""  src={InfoCard.image} alt={InfoCard.name} />
+                    <ImageZoom className=""  src={InfoCard?.image} alt={InfoCard?.name} />
                 </div>
               
             </div>
 
             {/* Text div */}
             <div className="space-y-3">
-                <h1 className="text-3xl font-bold">{InfoCard.name}</h1>
+                <h1 className="text-3xl font-bold">{InfoCard?.name}</h1>
                 
-                      <p className="text-3xl text-orange-600 ">ট {InfoCard.discount_price}</p>
+                      <p className="text-3xl text-orange-600 ">ট {InfoCard?.discount_price}</p>
                       <div className="flex gap-3">
                         <p className="text-sm text-gray-600 line-through">ট {InfoCard?.price}</p>
                         <p className="text-sm">
