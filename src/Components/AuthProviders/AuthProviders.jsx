bab5a1e0f2bcaa0575722c10144ae41a4b2507c5
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-23 17:45:51 +0600

    bug fixed on Authproviders

--- src/Components/AuthProviders/AuthProviders.jsx
+++ src/Components/AuthProviders/AuthProviders.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-// import app from "../FireBase/Firebase";
+
 import app from "../FireBase/firebase"
 
 
