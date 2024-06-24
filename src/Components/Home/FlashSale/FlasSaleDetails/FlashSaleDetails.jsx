96c1da57f407ccb7718211cdd0f3089befc578f5
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-24 22:38:52 +0600

    add to cart functionality added

--- src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
+++ src/Components/Home/FlashSale/FlasSaleDetails/FlashSaleDetails.jsx
@@ -2,14 +2,80 @@ import { useLoaderData, useParams } from "react-router-dom";
 import ImageZoom from "./ImageZoom";
 import AddComment from "../AddComment/AddComment";
 import Header from "../../Header/Header";
+import useAxiosPublic from "../../../Shared/Hooks/useAxiosPublic/useAxiosPublic";
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProviders/AuthProviders";
+import Swal from "sweetalert2";
 
 const FlashSaleDetails = () => {
     const CardsInfo = useLoaderData();
+    const AxiosPublic = useAxiosPublic();
+    const { user } = useContext(AuthContext);
     const { id } = useParams();
 
     const InfoCard = CardsInfo?.result?.find(brand => brand._id === id);
 
     const DiscountPercentage = Math.round(((InfoCard?.price - InfoCard?.discount_price) / InfoCard?.price) * 100);
+
+    const handleAddToCart = async () => {
+        try {
+            const res = await AxiosPublic.post('/cart', {
+                productCode: InfoCard.code,
+                name: InfoCard.name,
+                image: InfoCard.image,
+                price: InfoCard.price,
+                discountPrice: InfoCard.discount_price,
+                discountPercentage: DiscountPercentage,
+                details: InfoCard.details,
+                seller_email:InfoCard.seller_email,
+                customer_email:user?.email,
+                productSize:InfoCard.size,
+                rating:InfoCard.rating
+            });
+            console.log('Added to cart:', res.data);
+            if(res.data.insertedId){
+                
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Add to cart Successfully`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        } catch (error) {
+            console.error('Error adding to cart:', error);
+            Swal.fire({
+                position: "top-end",
+                icon: "error",
+                title: `Error:${error}`,
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }
+    };
+
+    const handleBuyNow = async () => {
+        try {
+            const res = await AxiosPublic.post('/buy', {
+                productCode: InfoCard.code,
+                name: InfoCard.name,
+                image: InfoCard.image,
+                price: InfoCard.price,
+                discountPrice: InfoCard.discount_price,
+                discountPercentage: DiscountPercentage,
+                details: InfoCard.details,
+                seller_email:InfoCard.seller_email,
+                customer_email:user?.email,
+                productSize:InfoCard.size,
+                rating:InfoCard.rating
+            });
+            console.log('Purchased:', res.data);
+            
+        } catch (error) {
+            console.error('Error purchasing:', error);
+        }
+    };
 
     return (
         <div>
@@ -17,7 +83,7 @@ const FlashSaleDetails = () => {
             <div className="flex mt-40 ml-40">
             <div className="relative mr-10 ">
             <div className="block lg:hidden">
-                    <img className="w-80 h-full object-cover" src={InfoCard?.image} alt={InfoCard?.name} />
+                    <img className="w-80 h-full object-cover shadow-xl bg-slate-200" src={InfoCard?.image} alt={InfoCard?.name} />
                 </div>
                 <div className="hidden lg:block w-80 h-96 border hover:border-orange-500 p-5">
                     <ImageZoom className=""  src={InfoCard?.image} alt={InfoCard?.name} />
@@ -41,8 +107,10 @@ const FlashSaleDetails = () => {
 
                       {/* Button */}
                       <div className="flex gap-10">
-                        <button className="w-1/4 border py-2 text-lg font-bold rounded-md border-orange-600 hover:bg-orange-600 hover:shadow-xl hover:text-white">Add To Cart</button>
-                        <button className="w-1/4 border py-2 text-lg font-bold rounded-md border-blue-600 hover:bg-blue-600 hover:shadow-xl hover:text-white">Buy Now</button>
+                        <button onClick={handleAddToCart}
+                         className="w-1/4 border py-2 text-lg font-bold rounded-md border-orange-600 hover:bg-orange-600 hover:shadow-xl hover:text-white">Add To Cart</button>
+                        <button onClick={handleBuyNow}
+                        className="w-1/4 border py-2 text-lg font-bold rounded-md border-blue-600 hover:bg-blue-600 hover:shadow-xl hover:text-white">Buy Now</button>
                       </div>
             </div>
 
