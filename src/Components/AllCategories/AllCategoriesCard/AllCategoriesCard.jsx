d773284318c7502e608274e0a05f1d8427aba61d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-25 18:41:54 +0600

    design updated on all category section

--- src/Components/AllCategories/AllCategoriesCard/AllCategoriesCard.jsx
+++ src/Components/AllCategories/AllCategoriesCard/AllCategoriesCard.jsx
@@ -5,10 +5,10 @@ const AllCategoriesCard = ({categorie}) => {
     const {image,category,title,} = categorie || {}
 
     return (
-        <div className="mt-10 mb-20 mr-5">
+        <div className="mt-10 mb-20 mr-5 hover:border-orange-500 hover:shadow-xl relative">
             <Link to={`/${category}`}>
-            <div className="border-[1px]  hover:border-[1px] hover:shadow-xl hover:border-gray-200 card card-compact h-60  ">
-                <figure><img className="w-60 h-40" src={image} alt="" /></figure>
+            <div className="border-[1px]  hover:border-[1px] hover:shadow-xl hover:border-orange-500  relative  card card-compact h-60  ">
+                <figure><img className="w-60 h-40 hover:scale-110 transition-all" src={image} alt="" /></figure>
                 <div className="card-body">
                     <h2 className="text-lg font-medium text-center">{title}</h2>
                 </div>
