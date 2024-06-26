9a5076815b9d89682942b5747cd833e2b0664154
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-26 17:56:39 +0600

    bug fixed on AdminDashboard

---
+++ src/Components/AdminDashboard/AdminUploadProduct/AdminUploadProducts.jsx
@@ -0,0 +1,11 @@
+
+
+const AdminUploadProducts = () => {
+    return (
+        <div>
+            <h1>ghfg</h1>
+        </div>
+    );
+};
+
+export default AdminUploadProducts;
--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import AdminDashboard from "../AdminDashboard/AdminDashboard";
 import AllUsers from "../AdminDashboard/AllUsers/AllUsers";
 import AdminAllProducts from "../AdminDashboard/AdminAllProducts/AdminAllProducts";
 import AdminPRoductDetails from "../AdminDashboard/AdminProductsDetails/AdminPRoductDetails";
+import AdminUploadProducts from "../AdminDashboard/AdminUploadProduct/AdminUploadProducts";
 
 export const router = createBrowserRouter([
     {
@@ -91,6 +92,10 @@ export const router = createBrowserRouter([
           path:"/AdminDashboard/product/:id",
           element:<AdminPRoductDetails></AdminPRoductDetails>,
           loader: ()=>fetch('http://localhost:5000/allProducts')
+        },
+        {
+          path:"/AdminDashboard/NewProduct",
+          element:<AdminUploadProducts></AdminUploadProducts>
         }
 
       ]
