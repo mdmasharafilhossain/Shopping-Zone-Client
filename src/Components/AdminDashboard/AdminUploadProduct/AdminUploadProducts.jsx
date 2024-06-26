fce25b3fa47c339af097d12902736dec7673eecc
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-26 19:39:57 +0600

    upload product route added

--- src/Components/AdminDashboard/AdminUploadProduct/AdminUploadProducts.jsx
+++ src/Components/AdminDashboard/AdminUploadProduct/AdminUploadProducts.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../AuthProviders/AuthProviders";
 import useAxiosPublic from "../../Shared/Hooks/useAxiosPublic/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 
 const Imgae_hosting_key = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMAGE_HOSTING_KEY}`
@@ -10,36 +11,56 @@ const AdminUploadProducts = () => {
     const {user} = useContext(AuthContext)
     const { register, handleSubmit } = useForm();
     const axiosPublic = useAxiosPublic();
+    const AxiosPublic = useAxiosPublic();
+   
+
+    const {  data: AllProducts = [], isLoading,refetch} = useQuery({
+        queryKey: ['AllProducts'],
+
+        queryFn: async () => {
+            const res = await AxiosPublic.get('/allProducts')
+            console.log(res.data)
+            return res.data;
+
+        }
+
+    })
+  console.log(user?.email)
     const onSubmit = async (data) => {
         console.log(data);
         const ImageFile = {image:data.image[0]}
-       const res = await axiosPublic.post(Imgae_hosting_key,ImageFile,{
+        const res = await axiosPublic.post(Imgae_hosting_key,ImageFile,{
         headers: {
             'content-type': 'multipart/form-data'
         }
        });
        console.log(res.data);
        if(res.data.success){
-        const articleInfo = {
+        const ProductsInfo = {
+            code:AllProducts.length+1,
             image: res.data.data.display_url,
             name: data.name,
             color: data.color,
-            price: data.price,
-            discount_price: data.discount_price || 0,
+            price: parseFloat(data.price),
+            discount_price: parseFloat(data.discount_price) || 0,
             seller_email:user?.email,
             rating:4.4,
             size:data.size,
-            status:'pending'
+            brand:data.brand || "No Brand",
+            warranty:data.warranty || "",
+            details:data.details,
+            type:data.type,
+            category:data.category
 
         }
-        // console.log(articleInfo);
-        const ArticleRes = await axiosPublic.post('/article',articleInfo);
+        console.log(ProductsInfo);
+        const ArticleRes = await axiosPublic.post('/allProducts',ProductsInfo);
         console.log(ArticleRes.data);
         if(ArticleRes.data.insertedId){
             Swal.fire({
                 position: "top-end",
                 icon: "success",
-                title: "Article Added Successfully",
+                title: "Product Added Successfully",
                 showConfirmButton: false,
                 timer: 1500
               });
@@ -53,7 +74,7 @@ const AdminUploadProducts = () => {
                 <h2 className="text-xl md:text-4xl lg:text-4xl font-bold">Upload <span className='text-[#FF3811]'>Products</span></h2>
             </div>
             
-            <div className="mt-32 border ml-10">
+            <div className="mt-10 border-2 rounded-md border-orange-500 ml-10">
             
 
             <div className="px-10 py-10">
@@ -146,25 +167,25 @@ const AdminUploadProducts = () => {
                     <div className="flex gap-10">
                     <label className="form-control w-full ">
                         <div className="label">
-                            <span className="label-text">Your Email</span>
+                            <span className="label-text">Brand Name</span>
                             
                         </div>
                         <input 
-                        {...register("email")}
+                        {...register("brand")}
                          type="text" 
-                         defaultValue={user?.email} readOnly
+                         placeholder="If Any Brand" 
                          className="input input-bordered w-full" />
                         
                     </label>
                     <label className="form-control w-full ">
                         <div className="label">
-                            <span className="label-text">Enter Product Size*</span>
+                            <span className="label-text">Warranty</span>
                             
                         </div>
                         <input 
-                        {...register("size")}
+                        {...register("warranty")}
                          type="text" 
-                         placeholder="S,M,XL,XXL ect.."  
+                         placeholder="If Any Warranty"  
                          className="input input-bordered w-full" />
                         
                     </label>
@@ -172,33 +193,36 @@ const AdminUploadProducts = () => {
                     </div>
                     <label className="form-control w-full ">
                         <div className="label">
-                            <span className="label-text">Description</span>
+                            <span className="label-text">Details Of Your Product&</span>
                             
                         </div>
                         <input 
-                        {...register("description")}
+                        {...register("details")}
                          type="text" 
-                         placeholder="Description Of The Article Title"  required
-                         className="input input-bordered w-full " />
+                         placeholder="Details Of your product"  required
+                         className="input input-bordered w-full h-[100px]" />
                         
                     </label>
                     <div className="flex gap-5">
-                    <select {...register("tag")}
+                    <select {...register("type") }
                         className="select select-bordered w-full ">
-                        <option disabled selected>Select Tag</option>
-                        <option value="War">War</option>
-                        <option value="Nature">Nature</option>
-                        <option value="Technology">Technology</option>
-                        <option value="Sports">Sports</option>
+                        <option disabled selected>Select Type*</option>
+                        <option value="Men">Men</option>
+                        <option value="Women">Women</option>
+                        <option value="Kids">Kids</option>
+                        <option value="Accessories">Home Accessories</option>
+                        <option value="Others">Others</option>
 
                     </select>
-                    <select {...register("publisher")}
+                    <select {...register("category")}
                         className="select select-bordered w-full ">
-                        <option disabled selected>Select Publisher</option>
-                        <option value="BBC">BBC</option>
-                        <option value="CNN">CNN</option>
-                        <option value="Aljajira">Aljajira</option>
-                        <option value="NDTV">NDTV</option>
+                        <option disabled selected>Select Category</option>
+                        <option value="Men">Men</option>
+                        <option value="Women">Women</option>
+                        <option value="Kids">Kids</option>
+                        <option value="Caps">Caps</option>
+                        <option value="Mobile">Mobile</option>
+                        <option value="Fan">Fan</option>
 
                     </select>
                     </div>
@@ -208,7 +232,7 @@ const AdminUploadProducts = () => {
                     type="file" 
                     className="file-input file-input-bordered w-full max-w-xs" />
                     </div>
-                    <input className="btn w-full bg-red-600 text-white" type="submit" />
+                    <input className="btn w-full bg-orange-600 text-white" type="submit" />
                 </form>
             </div>
         </div>
