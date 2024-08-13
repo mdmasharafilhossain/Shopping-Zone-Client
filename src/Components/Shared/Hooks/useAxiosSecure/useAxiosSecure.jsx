237602a8ccd4b0446d54c426789c330eaeb137fb
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-08-14 00:31:02 +0600

    bug fixed

--- src/Components/Shared/Hooks/useAxiosSecure/useAxiosSecure.jsx
+++ src/Components/Shared/Hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -2,8 +2,8 @@
 import axios from "axios";
 // https://shopping-zone-server-five.vercel.app/
 const AxiosSecure = axios.create({
-    // baseURL: "https://shopping-zone-server-five.vercel.app",
-    baseURL: "http://localhost:5000",
+    baseURL: "https://shopping-zone-server-five.vercel.app",
+    // baseURL: "http://localhost:5000",
 })
 const useAxiosSecure = () => {
     return AxiosSecure;
